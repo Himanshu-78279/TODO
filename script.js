68d34c61efb80d248bfad28aba6120837f824ab7
himanshu-78279 <himanshusharma78279@gmail.com>
2024-02-01 18:57:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let data = document.getElementById('data');
+let btn = document.getElementById('btn');
+// btn.addEventListener('click',(e)=>{
+//     console.log("hello");
+//     let text = data.value;
+//     e.preventDefault();
+//     document.body.innerHTML = `<li>${text}</li>`;
+// })
